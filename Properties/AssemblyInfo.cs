63434d704c994ffcae121d4f4363cc57367468ee
abulorianz <50519242+abulorianz@users.noreply.github.com>
2021-10-08 12:56:58 +0100

    Properties

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FemtoHelp Custom Controls")]
+[assembly: AssemblyDescription("Modern, flat, and elegant Custom Controls for Windows Forms By FemtoHelp")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("FemtoHelp")]
+[assembly: AssemblyProduct("FemtoHelp Controls")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("325d6422-b0a6-4859-8a68-cc4568c3e1e7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
